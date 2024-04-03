4510484a42817c920cf1df9b3565194b231b7602
V.Vitaliy <vitaliyverzyn25@gmail.com>
2024-04-03 17:22:26 +0300

    initialization

---
+++ src/main/java/com/example/patternimplementations/PatternImplementationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.patternimplementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternImplementationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternImplementationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/patternimplementations/PatternImplementationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patternimplementations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternImplementationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
